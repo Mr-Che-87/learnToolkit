4f14e127fa04cfcbb0782fb70e68fddcdcfc9ff3
Alexey Chechetkin <109634077+Mr-Che-87@users.noreply.github.com>
2024-04-21 22:21:51 +0300

    redux_meaining

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,10 +1,8 @@
-import { useRef } from "react";  //*необяз - для получения прямых ссылок на html
-import { useDispatch } from "react-redux";  //Dispatch из библиотеки redux
+import { useRef } from "react";  
+import { useDispatch } from "react-redux"; 
 
-import { addTodo } from "../features/todoSlice";  //Redux-Toolkit
+import { addTodo } from "../features/todoSlice"; 
 //import { addTodo } from "../old_redux_actions/actions";  //REDUX
-
-
 
 const Task = () => {
     const dispatch = useDispatch();
@@ -13,8 +11,8 @@ const Task = () => {
     //Обработчик клика кнопки "Добавить задачу":
     function addNewTask() {  //???????????????почему addNewTask не требует аргумент id, а «видит» его напрямую из {id: Date.now(), text: action.payload}, а handleDelete требует???????
       
-        const task = inputRef.current.value.trim();   //task = то, что введено в инпут (trim удаляет начальные и конечные пробелы)
-        if (task !== "") {  //если введённое не пустота, то:
+        const task = inputRef.current.value.trim();   
+        if (task !== "") {  
           
           //1 редуктор addTodo передаёт разные ВЕРСИИ состояния разным компонентам(и этому и в SelectDefaultTasks.jsx):
           dispatch(addTodo(task));  //диспатчим экшн addTodo (принимаем экшн с action.payload = inputRef.current.value)
